9a86a24c1e35324a280c7d63a3ef4f3ef0ef5a35
Arjun Devraj <arjundevraj@Arjuns-MBP-2.attlocal.net>
2021-01-18 09:48:52 -0600

    Added nls long exp stim

--- init_csv.py
+++ init_csv.py
@@ -3,9 +3,10 @@ import boto3
 import io
 
 available_list = []
-for i in range(102, 201, 2):
+for i in range(101, 200, 2):
 	available_list.append(int(i))
 print(available_list)
+print(len(available_list))
 s3 = boto3.resource('s3')
 obj = s3.Object('psy454', 'available_lists.csv')
 with io.StringIO() as buffer:
