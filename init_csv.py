3e5d66f1e8b8ad5ae26dedcfdca95a348ab8ecb1
Arjun Devraj <arjundevraj@Arjuns-MBP-2.attlocal.net>
2021-07-14 22:05:28 -0500

    add last files

--- init_csv.py
+++ init_csv.py
@@ -3,7 +3,7 @@ import boto3
 import io
 
 available_list = []
-for i in range(101, 901):
+for i in range(101, 1001):
 	available_list.append(int(i))
 print(available_list)
 print(len(available_list))
